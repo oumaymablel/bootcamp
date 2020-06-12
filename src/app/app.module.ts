996b12943be1d2b7e391dac7f67a36192052e5ce
oumaymablel <oumaymablel2@gmail.com>
2020-06-12 12:16:04 +0100

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HighlightsComponent } from './highlights/highlights.component';
+import { TeamComponent } from './team/team.component';
+import { InfoComponent } from './info/info.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HighlightsComponent,
+    TeamComponent,
+    InfoComponent,
+    FormComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
